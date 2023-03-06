aa114ae16ddd7d4ed2ffbdbcf70f53c0c50977c3
RorschachGi <tumanovAccount7773@rambler.ru>
2023-03-06 15:16:07 +0400

    Добавьте файлы проекта.

---
+++ LessonsCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonsCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "54";
+
+            int digit;
+
+            bool result = int.TryParse(str, out digit);   
+            if(result)
+            {
+                Console.WriteLine(digit);
+            }
+            else
+            {
+                Console.WriteLine("Конвертация не удалась");
+            }
+
+        }
+    }
+}
