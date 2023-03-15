64803711a780d6da50213a7fb9ba1f5bed83acb8
RorschachGi <tumanovAccount7773@rambler.ru>
2023-03-15 18:02:18 +0400

    WhileLesson commit

---
+++ Lesson4_WhileLesson/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4_WhileLesson
+{
+    /*
+     * Цикд while и do while
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //while
+            int count = 0;
+            int limit = int.Parse(Console.ReadLine());
+            while (count < limit)
+            {
+                Console.WriteLine(count);
+                count++;
+            }
+
+            //do while cначала выполняет тело цикла, а затем проверяет условие 
+            int countSecond = 0;
+            do
+            {
+                countSecond++;
+                Console.WriteLine(countSecond);
+            } while (countSecond < 5);
+
+
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
