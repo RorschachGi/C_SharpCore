95a94dcaa1d6748b13573959d9a6c67186143802
RorschachGi <tumanovAccount7773@rambler.ru>
2023-03-24 13:51:30 +0400

    add lesson "Ternary Operator"

---
+++ Lesson6_NestedLoops/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6_NestedLoops
+{
+    internal class Program
+    {
+
+        //Создание случайного двумерно массива
+        static int[,] GetRandomArray(int rows, int columns)
+        {
+            Random random = new Random();
+
+            var result = new int[rows, columns];
+
+            //Перебираем двойной массив и заполняем его случайными числами
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    result[i, j] = random.Next(100);
+                }
+            }
+
+            return result;
+        }
+
+        //Вывод двумерно массива в консоль
+        static void PrintArray(int[,] array, int rows, int columns)
+        {
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int rows = 5;
+
+            int columns = 10;
+
+            var myArray = GetRandomArray(rows, columns);
+
+            PrintArray(myArray, rows, columns);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson6_NestedLoops/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lesson6_NestedLoops")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lesson6_NestedLoops")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8d3bf344-0461-4ffd-a233-64b2ca4d0031")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
