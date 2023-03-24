e1cc89d7644867f2e62b40776955150b090a5ae0
RorschachGi <tumanovAccount7773@rambler.ru>
2023-03-24 13:55:47 +0400

    add lesson "TernaryOperator"

---
+++ Lesson7_TernaryOperator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_TernaryOperator
+{
+    /*
+     * 
+     * Тернарный оператор
+     * 
+     * [первый оператор - условие] ? [второй операнд if_true] : [третий операнд if_false]
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool accessAllowed;
+
+            string password = "qwerty";
+
+            string enteredPassword = Console.ReadLine();
+
+            //Обычная конструкция 
+            /*
+            if(password == enteredPassword)
+            {
+                accessAllowed = true;
+            }
+            else
+            {
+                accessAllowed = false;
+            }*/
+
+            //Синтаксис тернартного оператора
+            accessAllowed = (password == enteredPassword) ? true : false;
+
+            
+
+            Console.WriteLine(accessAllowed);
+
+            Console.ReadLine();
+        }
+    }
+}
